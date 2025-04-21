bea0f2b12f06adc7eacb9132464fbbca7339de7d
Artur Sabanadze <diakosmisi26@hotmail.com>
2025-04-21 21:40:00 +0200

    upup

--- src/components/LoginForms/LoginForm.jsx
+++ src/components/LoginForms/LoginForm.jsx
@@ -5,6 +5,7 @@ import './LoginForm.css'; // Add your styles here
 const LoginForm = ( {cssID, onSubmit, buttonText = 'Login', errorMessage} ) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false)
     const cssIDhandler = () => cssID || '1';
 
     const handleSubmit = (e) => {
@@ -13,6 +14,10 @@ const LoginForm = ( {cssID, onSubmit, buttonText = 'Login', errorMessage} ) => {
             onSubmit({ email, password });
         }
     };
+
+    const handleCheck= (event) => {
+        setRemember(event.target.checked);
+      };
 
     return (
         <form id={"login-form-"+cssIDhandler()} onSubmit={handleSubmit}>
@@ -41,6 +46,22 @@ const LoginForm = ( {cssID, onSubmit, buttonText = 'Login', errorMessage} ) => {
                 />
             </div>
             {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+            <div id={`forgot-password-link-${cssIDhandler()}`}>
+  <label htmlFor="remember">
+    <input 
+      type="checkbox"
+      name="remember"
+      id="remember"
+      checked={remember}
+      onChange={handleCheck}
+    />
+    Remember me
+  </label>
+  <a href="#">Forgot password</a>
+ </div>
+
+            
             <button type="submit" id={"login-button-"+cssIDhandler()}>
                 {buttonText}
             </button>
